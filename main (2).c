f50609fa3ce766e178b6482036208aa97c5ebb60
FTizzi <103120192+FTizzi@users.noreply.github.com>
2024-03-07 19:05:56 +0100

    Update main (2).c

--- main (2).c
+++ main (2).c
@@ -1,10 +1,5 @@
 /******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
 *******************************************************************************/
 #include <stdio.h>
 
@@ -28,10 +23,10 @@ if (scelta =='B')
 }
     while (scelta =='A')
     {
-        scanf ("%c", &scelta);      
+        scanf ("%c", &scelta);    
        gioca_partita();
   
-    scanf ("%c", &scelta);
+   
       
     }
  
@@ -41,8 +36,8 @@ if (scelta =='B')
 void print_menu ()
 
 {
-    printf("Start Menu:\n");
-    printf("A >> NG\nB >>EXIT\n");
+    printf("\n\n*******************************************************************************\n\nStart Menu:\n\n");
+    printf("A >> NG\nB >>EXIT\n\n");
     
 }
 
@@ -50,62 +45,108 @@ int gioca_partita()
 {
     
     int anime = 0;
-    char Nxxxxx_Man;
+    char Nxxxxx_Man,Pupy,Ultar;
     
-    printf ("Q1:\n");
+    printf ("\nQ1:\n\n");
     printf ("Come si chiamava il gatto di H.P.Lovecraft?\n");
-    printf ("A >> Pupy\nB >> Nxxxxx_Man\nC >> Ultar\n");
-    scanf ("%c", &Nxxxxx_Man);
+    printf ("A >> Pupy\nB >> Nxxxxx_Man\nC >> Ultar\n\n");
+    scanf (" %c", &Nxxxxx_Man);
     
     if (Nxxxxx_Man == 'B')
     {
         
 anime =anime+1000;
-    
-        
-   
-    
-    printf ("anime %d\n", anime);
+
+ printf ("\nAZATOTH CONTINUA A SOGNARE\n\n");
     
      }
+     else {
+          printf ("\nHai Risvegliato Cthulhu\n\n");
+     }
+
+ 
+     
      {
-     char PEFFFFOOOZZZAAA;
+     char DOOM_GUY;
     
-    printf ("Q2:\n");
+    printf ("\nQ2:\n\n");
     printf ("Quale'è il nemico dei demoni?\n");
-    printf ("A >> Bho\nB >> Bad_Guy\nC >> DOOM_GUY\n");
-    scanf ("%c", &PEFFFFOOOZZZAAA);
+    printf ("A >> Bho\nB >> Bad_Guy\nC >> DOOM_GUY\n\n");
+    scanf (" %c", &DOOM_GUY);
     
-    if (PEFFFFOOOZZZAAA=='C') 
+    if (DOOM_GUY=='C') 
     {
         
 anime =anime+1000;
 
-    printf ("anime %d\n", anime);
+  printf ("\nDemoni Eliminati\n\n");
+}
+else {
+          printf ("\nTorna a giocare a The Simps\n\n");
 }
 }
-
      {
      char IL_SOLE;
     
-    printf ("Q3:\n");
+    printf ("\nQ3:\n\n");
     printf ("Cosa si loda?\n");
-    printf ("A >> IL_SOLE\nB >> Budda\nC >> Dio\n");
-    scanf ("%c", &IL_SOLE);
+    printf ("A >> IL_SOLE\nB >> Budda\nC >> Dio\n\n");
+    scanf (" %c", &IL_SOLE);
     
     if (IL_SOLE=='A')
     {
         
 anime =anime+1000;
 
-    printf ("anime %d\n", anime);
+    
+     printf ("\nANIME ASSORBITE\n\n");
 }
+else {
+          printf ("\nShottato dal Demone Capra\n\n");
+     }
+         
+     }
+     
+      {
+     char ZA_WAAAARDO;
+    
+    printf ("\nQ4:\n");
+    printf ("Quale'è il potere di DIO BRANDO?\n");
+    printf ("A >> La Forza\nB >> Bloccare_il_Tempo\nC >> ZA_WAAAARDO\n\n");
+    scanf (" %c", &ZA_WAAAARDO);
+    
+    if (ZA_WAAAARDO=='C')
+    {
+        
+anime =anime+1000;
 
+  printf ("\nSTANDO PAWAH!!!\n");
 }
-printf ("GRASIAS SENOR\n");
+else {
+          printf ("\nMUDA MUSA MUDA!!!\n\n");
+}
+}
+   {
+     char De;
+    
+    printf ("\nQ5:\n");
+    printf ("Cosa dice sempre in Darion?\n");
+    printf ("A >> E Quindi\nB >> B Daaai\nC >> De'\n\n");
+    scanf (" %c", &De);
+    
+    if (De=='C')
+    {
+        
+anime =anime+1000;
+
+  printf ("\nPEFFFOZZAAA!!!!\n\n");
+}
+else {
+          printf ("\nDio Meo!\n\n");
+}
+}
+printf ("GRASIAS SENOR\n \n \n");
+printf ("ANIME %d\n", anime);
+ printf("\n\n*******************************************************************************\n\n");
 return 0;
 }
-
-
-
-    
